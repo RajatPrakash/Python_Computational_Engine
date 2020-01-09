1741d98bd9bae75af8b38061daf9cb77cddb2626
Rajat Prakash <rajatprakash904@gmail.com>
2020-01-09 09:40:46 +0530

    Minor changes has been done

--- Assistance.py
+++ Assistance.py
@@ -1,13 +1,10 @@
 import wolframalpha
 import wikipedia
 
-while True:
-    user_input = input('Type Your Query! ')
-    try:
-        app_id = 'Q3K2GK-2E3XXXXXXX'
-        client = wolframalpha.Client(app_id)
-        answer = client.query(user_input)
-        result = next(answer.results).text
-        print(result)
-    except:
-        print(wikipedia.summary(user_input))
+
+user_input = input('Type Your Query! ')
+app_id = 'Q3K2GK-2E37H9ERUE'
+client = wolframalpha.Client(app_id)
+answer = client.query(user_input)
+result = next(answer.results).text
+print(result)
